1cdde57e247634707cc09ec497fdec42ed09a69f
Sergey <61564019+Gordievskih@users.noreply.github.com>
2021-03-25 19:28:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+		function init(div) {
+    //Вырезаем блок который хотим переместить
+	  		var Block = $('div-class(id) который нужно переместить').detach();
+        //вставляем блок последним ребенком блока div - который мы получаем пареметром в функцию при вызове
+	  		Block.appendTo(div);	
+	};
+  //Условие и событие при котором вызываем функцию
+		$(window).width() < 1023 ? $( init('.lcontacts') ) : $( init('.lresmenu') );
+		window.onresize = function(event) {
+			$(window).width() < 1023 ? $( init('.lcontacts') ) : $( init('.lresmenu') );
+	};
